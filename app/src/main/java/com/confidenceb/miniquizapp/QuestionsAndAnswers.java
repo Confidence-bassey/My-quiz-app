49b2197c66f26fe864032e9442afa2bc2aef34e0
Confidence-bassey <confidencebass2@gmail.com>
2022-10-28 16:53:38 +0100

    Created string arrays for the question, answers and options

--- app/src/main/java/com/confidenceb/miniquizapp/QuestionsAndAnswers.java
+++ app/src/main/java/com/confidenceb/miniquizapp/QuestionsAndAnswers.java
@@ -2,5 +2,22 @@ package com.confidenceb.miniquizapp;
 
 public class QuestionsAndAnswers {
 
+    public static String[] questions = {
+            "Which company owns android",
+            "which of the following is not a programming language",
+            "Which channel is strickly meant for videos"
+    };
+
+    public static String[][] options = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Wordpress","Dart"},
+            {"Facebook","WhatsApp","Twitter","Youtube"},
+    };
+
+    public static String[] answers = {
+            "Google",
+            "Wordpress",
+            "Youtube"
+    };
 
 }
